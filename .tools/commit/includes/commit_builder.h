c3686b906140463fac0c3ae22ddda8e7de06bc63
Gopmyc <gopmyc.pro@gmail.com>
2025-12-12 00:32:00 +0100

    chore(.tools/commit/includes/commit_builder.h): 🔧 - Updated file (commit_builder.h)

--- .tools/commit/includes/commit_builder.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMIT_BUILDER_H
-# define COMMIT_BUILDER_H
-
-# include <stddef.h>
-#include "auto_commit.h"
-
-char *strBuildCommitMessage(const char *sPath, t_commit_type eType);
-
-#endif
