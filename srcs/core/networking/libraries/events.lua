86d6cb38d2c01cf23ff1454e6638c5cc2e7a1c6e
Gopmyc <hoyaux.gaspard@gmail.com>
2026-01-21 13:13:56 +0100

    refactor(srcs/core/networking/libraries/events.lua): ♻️ - Added core base systems (events.lua)

---
+++ srcs/core/networking/libraries/events.lua
@@ -0,0 +1,55 @@
+function LIBRARY:Initialize(tEvents)
+	local tEvents = tEvents or {}
+
+	return setmetatable(
+		{
+			__EVENTS = setmetatable(
+				{},
+				{
+					__index = function(_, kKey)
+						return tEvents[kKey]
+					end,
+					__newindex = function()
+						error("__EVENTS is read-only", 2)
+					end,
+					__pairs = function()
+						return pairs(tEvents)
+					end,
+					__len = function()
+						local iCount = 0
+						for _, _ in pairs(tEvents) do
+							iCount = iCount + 1
+						end
+						return iCount
+					end
+				}
+			),
+		},
+		{
+			__mode		= "kv",
+		}
+	)
+end
+
+function LIBRARY:Call(tServer, tEvent)
+	return xpcall(
+		function()
+			return self.__EVENTS[tEvent.sType](self, tServer, tEvent)
+		end,
+		function(sErr)
+			MsgC(Color(255, 0, 0), "[ERROR] Event error : " .. tostring(sErr))
+		end
+	)
+end
+	
+function LIBRARY:BuildEvent(sType, tPeer, Data, iChannel)
+	assert(isstring(sType),	"BuildEvent : Type event must be a string")
+	assert(istable(tPeer),	"BuildEvent : Peer event must be a table")
+		
+	return {
+		sType		= sType,
+		tPeer		= tPeer,
+		tData		= Data,
+		iChannel	= iChannel,
+	}
+end
