2c8c15c5b4eed606bcaaefe49d91da9909c5c702
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-06 17:14:48 +0100

    chore(main.lua): 🔧 - Updated file ()

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
 local ArcNet = require("srcs")
 
 function lovr.load() end
-function lovr.update(iDeltaTime) end
-function lovr.draw(Pass) end
+function lovr.update(iDeltaTime)
+	ArcNet:Update(iDeltaTime)
+end
+
+function lovr.draw(Pass)
+	ArcNet:Draw(Pass)
+end
