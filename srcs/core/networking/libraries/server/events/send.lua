c42b120a90de9e961b5cb6540461754fdd3477f2
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-11 02:25:40 +0100

    refactor(srcs/core/networking/libraries/server/events/send.lua): ♻️ - update core logic (send.lua)

--- srcs/core/networking/libraries/server/events/send.lua
+++ srcs/core/networking/libraries/server/events/send.lua
@@ -1,23 +1,10 @@
 function LIBRARY:Call(tServer, tEvent)
 	local sID		= tostring(tEvent.udPeer:connect_id())
 	local udPeer	= tServer:IsValidClient(sID)
-	local tData		= tEvent.tData
-	local sFlag		= tData.flag
 
 	if not udPeer then
 		return MsgC(Color(231, 76, 60), "Attempted to send message to unregister Client [ID : " .. sID .. "]  : " .. tostring(udPeer))
 	end
-
-	if not istable(tData) then
-		return MsgC(Color(231, 76, 60), "Attempt to send data type other than table"..type(tData))
-	end
 		
-	tData	=	{tData.id, tData.packet}
-				
-	---- pcall all steps ----
-	-- // TODO : Table data to JSON...
-	-- // TODO : Crypt data...
-	-- // TODO : Compress data..
-		
-	udPeer:send(tData, tEvent.iChannel, sFlag or "reliable")
+	udPeer:send(tServer.CODEC:Encode(tEvent.Data), tEvent.iChannel, tEvent.sFlag)
 end
