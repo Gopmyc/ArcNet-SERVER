4e375ebdaa703d4aef81726c1b17759d1e86932e
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-12 04:37:38 +0100

    refactor(srcs/core/networking/libraries/server/events/send.lua): ♻️ - update core logic (send.lua)

--- srcs/core/networking/libraries/server/events/send.lua
+++ srcs/core/networking/libraries/server/events/send.lua
@@ -6,10 +6,5 @@ function LIBRARY:Call(tServer, tEvent)
 		return MsgC(Color(231, 76, 60), "Attempted to send message to unregister Client [ID : " .. sID .. "]  : " .. tostring(tPeer[1]))
 	end
 
-	local Encoded = tServer.CODEC:Encode(tEvent.Data)
-
-	print(Encoded)
-	print(#Encoded)
-		
-	tPeer[1]:send(Encoded, tEvent.iChannel, tEvent.sFlag)
+	tPeer[1]:send(tServer.CODEC:Encode(tEvent.Data), tEvent.iChannel, tEvent.sFlag)
 end
