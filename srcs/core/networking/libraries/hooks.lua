5a8b3e8528d8f2a6bf3ab7fe40b0f8f365a494b6
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-12 23:25:45 +0100

    refactor(srcs/core/networking/libraries/hooks.lua): ♻️ - update core logic (hooks.lua)

--- srcs/core/networking/libraries/hooks.lua
+++ srcs/core/networking/libraries/hooks.lua
@@ -11,8 +11,8 @@ function LIBRARY:Initialize()
 end
 	
 function LIBRARY:AddHook(sID, fCallBack)
-	assert(isstring(sID),			"[ERROR] 'AddHook' : hook ID must be a string")
-	assert(isfunction(fCallBack),	"[ERROR] 'AddHook' : callback must be a function")
+	assert(IsString(sID),			"[ERROR] 'AddHook' : hook ID must be a string")
+	assert(IsFunction(fCallBack),	"[ERROR] 'AddHook' : callback must be a function")
 		
 	self.__HOOKS[sID]						= self.__HOOKS[sID] or {}
 	self.__HOOKS[sID][#self.__HOOKS[sID] + 1]	= fCallBack
@@ -21,15 +21,15 @@ function LIBRARY:AddHook(sID, fCallBack)
 end
 
 function LIBRARY:CallHook(sID, Data)
-	assert(isstring(sID),	"[ERROR] 'CallHook' : hook ID must be a string")
+	assert(IsString(sID),	"[ERROR] 'CallHook' : hook ID must be a string")
 	assert(Data ~= nil,		"[ERROR] 'CallHook' : data must be provided")
 
-	if not (istable(self.__HOOKS[sID]) and next(self.__HOOKS[sID])) then
+	if not (IsTable(self.__HOOKS[sID]) and next(self.__HOOKS[sID])) then
 		return MsgC(Color(255, 0, 0), "[ERROR] 'CallHook' : no hooks found for ID '" .. sID)
 	end
 		
 	for iID, fCallBack in ipairs(self.__HOOKS[sID]) do
-		if not isfunction(fCallBack) then goto continue end
+		if not IsFunction(fCallBack) then goto continue end
 
 		fCallBack(Data)
 
@@ -38,9 +38,9 @@ function LIBRARY:CallHook(sID, Data)
 end
 	
 function LIBRARY:RemoveHook(sID, iID)
-	assert(isstring(sID), "[ERROR] 'RemoveHook' : hook ID must be a string")
+	assert(IsString(sID), "[ERROR] 'RemoveHook' : hook ID must be a string")
 		
-	if isnumber(iID) then
+	if IsNumber(iID) then
 		return table.remove(self.__HOOKS[sID], iID)
 	end
 
@@ -48,11 +48,11 @@ function LIBRARY:RemoveHook(sID, iID)
 end
 
 function LIBRARY:Destroy()
-	if not istable(self) then return end
+	if not IsTable(self) then return end
 
-	if istable(self.__HOOKS) then
+	if IsTable(self.__HOOKS) then
 		for sID, tList in pairs(self.__HOOKS) do
-			if istable(tList) then
+			if IsTable(tList) then
 				for i = 1, #tList do
 					tList[i] = nil
 				end
