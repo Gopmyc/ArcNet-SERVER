648e1db036a7ba7c92595c15ff24cbf85c9c94b1
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-12 23:25:49 +0100

    chore(env/LoadFileInEnvironment/init.lua): ➕ - add miscellaneous file (init.lua)

---
+++ env/LoadFileInEnvironment/init.lua
@@ -0,0 +1,15 @@
+return function(sPath, tEnvironment)
+	local sCode					= lovr.filesystem.read(sPath)
+	if not sCode then
+		return MsgC(Color(231, 76, 60), "[ENV-LOADER] Cannot read file: " .. sPath)
+	end
+
+	local fChunk, sCompileErr	= loadstring(sCode, sFileSource)
+	if not fChunk then
+		return MsgC(Color(231, 76, 60), "[ENV-LOADER] Compile error: " .. tostring(sCompileErr) .. " in file: " .. sPath)
+	end
+
+	setfenv(fChunk, tEnvironment)
+
+	return fChunk
+end
