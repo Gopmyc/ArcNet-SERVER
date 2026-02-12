848fc8fccd2a1010115814b166d2fa0cafa2449a
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-12 04:32:36 +0100

    refactor(srcs/core/networking/libraries/client/events/receive.lua): ♻️ - update core logic (receive.lua)

--- srcs/core/networking/libraries/client/events/receive.lua
+++ srcs/core/networking/libraries/client/events/receive.lua
@@ -1,12 +1,6 @@
 function LIBRARY:Call(tClient, tEvent)
-	local sID		= tostring(tEvent.udPeer:connect_id())
-	local udPeer	= tClient:IsValidClient(sID)
-	
-	if not udPeer then
-		return MsgC(Color(231, 76, 60), "Unregister Client [ID : " .. sID .. "] attempted to send message : " .. tostring(tEvent.Data))
-	end
-
+	local sID					= tostring(tEvent.udPeer:connect_id())
 	local sPacketID, Content	= tClient.CODEC:Decode(tEvent.Data)
 
-	tClient.HOOKS:CallHook(sPacketID, Content)
+	return tClient.HOOKS:CallHook(sPacketID, Content)
 end
